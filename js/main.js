8f5d19a326851ee46d6a72aba3b9ac2b0771b5a9
Arthur-Volkov <bafequan@gmail.com>
2021-02-01 18:58:26 +0200

    first steps refactoring and adding content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
 console.log('Starting up');
+
+
+$(document).ready(function() {
+    renderProjects()
+})
+
+
+function renderProjects() {
+    var projects = getProjects();
+    var strHTML = projects.map(function (project, idx) {
+        return `<div class="col-md-4 col-sm-6 portfolio-item">
+                  <a onclick="renderModal(${idx})" class="portfolio-link" data-toggle="modal" href="#portfolioModal">
+                    <div class="portfolio-hover">
+                      <div class="portfolio-hover-content">
+                        <i class="fa fa-plus fa-3x"></i>
+                      </div>
+                    </div>
+                    <img class="img-fluid" src="img/portfolio/${idx+1}-thumbnail.jpg" alt="">
+                  </a>
+                  <div class="portfolio-caption">
+                    <h4>${project.name}</h4>
+                    <p class="text-muted">${project.title}</p>
+                  </div>
+                </div>`
+    }).join('');
+    $('.inject-projects').html(strHTML);
+}
+
+
+function renderModal(idx) {
+    var project = getProjects()[idx];
+    var strHTML = `<h2>${project.name }</h2>
+                   <p class="item-intro text-muted">${project.title}.</p>
+                   <img class="img-fluid d-block mx-auto" src="${project.imgUrl}" alt="">
+                   <p>${project.desc}</p>
+                   <ul class="list-inline">
+                     <li>Date: January 2017</li>
+                     <li>Client: Threads</li>
+                     <li>Category: Illustration</li>
+                   </ul>
+                   <a class="btn btn-primary" href="${project.url}" target="_blank">Open The Project</a>
+                   <button class="btn btn-primary" data-dismiss="modal" type="button">
+                       <i class="fa fa-times"></i>
+                       Close details</button>`
+    $('.modal-body').html(strHTML)
+}
---
+++ js/portfolio-service.js
@@ -0,0 +1,70 @@
+'use strict'
+
+var gProjects = [
+    {
+        id: 'mineswiper',
+        name: 'Mineswiper',
+        title: 'Mineswaper Game using Vanila JS',
+        desc: 'Mineswaper Game using Vanila JS with some cool features, and more...',
+        imgUrl: 'img/portfolio/1-full.jpg',
+        url: 'https://arthurvolkov.github.io/Mine-Sweeper/',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    {
+        id: 'bookshop',
+        name: 'Bookshop',
+        title: 'Bookshop demo web-site using Vanila JS',
+        desc: 'Bookshop demo web-site using Vanila JS, and more...',
+        imgUrl: 'img/portfolio/2-full.jpg',
+        url: 'https://arthurvolkov.github.io/Bookshop/',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    {
+        id: 'packman',
+        name: 'Packman',
+        title: 'Packman Game using Vanila JS',
+        desc: 'Packman Game using Vanila JS with some cool features, and more...',
+        imgUrl: 'img/portfolio/3-full.jpg',
+        url: 'https://arthurvolkov.github.io/packman/',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    {
+        id: 'guessMe',
+        name: 'Guess Me',
+        title: 'Guess me" - selflearning game using JS',
+        desc: '"Guess me" - selflearning game using JS, JQuery and Bootstrap',
+        imgUrl: 'img/portfolio/4-full.jpg',
+        url: 'https://arthurvolkov.github.io/guess-me/',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    {
+        id: 'ballGame',
+        name: 'Ball Game',
+        title: 'Simple ball-catch game',
+        desc: 'Simple ball-catch game using Vanila JS, on second week of learing JS',
+        imgUrl: 'img/portfolio/5-full.jpg',
+        url: 'https://arthurvolkov.github.io/ball-game/',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    {
+        id: 'bookshop',
+        name: 'Bookshop',
+        title: 'Bookshop demo web-site using Vanila JS',
+        desc: 'Bookshop demo web-site using Vanila JS, and more...',
+        imgUrl: 'img/portfolio/6-full.jpg',
+        url: 'https://arthurvolkov.github.io/Mine-Sweeper/',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+    },
+]
+
+
+
+function getProjects() {
+    return gProjects.slice();
+}
